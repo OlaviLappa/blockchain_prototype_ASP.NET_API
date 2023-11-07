b8a7795be6be67dcc9f9e51242fc001c88c74f29
OlaviLappa <tomas.shellenberg@mail.ru>
2023-11-07 17:34:59 +0300

    add class WalletBalanceController, TokenTransferRequest, and add method GetBalance

---
+++ blockchain_test_API/Modules/TokenTransferRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace blockchain_test_API.Modules
+{
+    public class TokenTransferRequest
+    {
+        public string FromAddress { get; set; }
+        public string SenderPrivateKey { get; set; }
+        public string SenderPublicKey { get; set; }
+        public decimal SenderBalance { get; set; }
+        public string ToAddress { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
